b8825c0e64c8721d3850b524839bbc447a6f67cc
FatFang <gbossfamily2@gmail.com>
2022-08-11 11:09:17 +0800

    new

--- 文化部各分類縮圖抓取-可換頁版/imageCatch.py
+++ 文化部各分類縮圖抓取-可換頁版/imageCatch.py
@@ -21,34 +21,39 @@ def mkdir(path):
         # 如果目錄已存在，則不建立，提示目錄已存在
         print(f'{path} is exist')
 
-# https://collections.culture.tw/Object.aspx?RNO=Q0NDSzU2OTI0MDA1&amp;SYSUID=31
+
 # 功能：下載圖片
+# argument: url_image(縮圖網址) organ(該藏品收藏之館名) name(該藏品之名稱) save_path(該藏品之儲存路徑)
 def getimage(url_image, organ, name, save_path):
     img_ = requests.get("https://collections.culture.tw/" + url_image)  # 網頁requests並回傳圖片
     img_name = save_path + organ + '-' + name  # 檔案名稱(路徑/機構-名稱[順序].jpg)
     # print(save_path)
-    print(img_name)  # check filename
-
+    # print(img_name)  # check filename
     with open(img_name + '.jpg', "wb") as file:  # 創建檔案
         file.write(img_.content)  # 寫入檔案
 
 
 # 造訪各類別網頁
+# argument: urlss(該分類藏品總合集的網址) path(縮圖儲存之資料夾路徑) organize_class_name(該分類之名稱)
 def each(urlss, path, organize_class_name):
     print(path)
-    path_tmp = []
+    path_tmp = []  # 給多執行緒的路徑list
     driver.get(urlss)  # 程式自動開啟Google Chrome前往urlss其網址
-    driver.find_element(By.XPATH,
-                        "//*[@id='GridView1']/tbody/tr/td/table/tbody/tr/td[7]/input").click()  # 點擊頁數快捷鍵投鍵，前往最後一頁
-    count = int(
-        driver.find_element(By.XPATH, '//*[@id="GridView1"]/tbody/tr/td/table/tbody/tr/td[7]/span').text)  # 取得此類別共有多少頁
+
+    driver.find_element(By.XPATH,"//*[@id='GridView1']/tbody/tr/td/table/tbody/tr/td[7]/input").click()  # 點擊頁數快捷鍵投鍵，前往最後一頁
+    count = int(driver.find_element(By.XPATH, '//*[@id="GridView1"]/tbody/tr/td/table/tbody/tr/td[7]/span').text)  # 取得此類別共有多少頁
     print(f'This class has {count} pages')
     driver.find_element(By.XPATH, '//*[@id="GridView1"]/tbody/tr/td/table/tbody/tr/td[1]/input').click()  # 返回第一頁
-    button_pos = 1  # 控制按鈕td之位置
+
+    button_pos = 1  # 控制按鈕<td>之位置
     page_control = True  # 第一頁與其他頁不同 設此布林值去控制
-    broken_page = False
+    broken_page = False  # 是否有破圖網頁(經證實在建築圖此分類下第1246頁是有問題的 -> 網頁程式碼問題)
+    page_big_six = True  # 若總頁數小於五頁 -> 不需要去搜尋'...'按鍵
     if organize_class_name == '建築圖':
         broken_page = True
+    if count < 6:
+        page_big_six = False 
+
     while True:  # 下載圖片之迴圈
         # page(1)
         # td位置     1 2 3 4 5  6  7
@@ -62,16 +67,16 @@ def each(urlss, path, organize_class_name):
         if button_pos > 8 and not page_control:  # 第二開始至最後一頁的程序
             button_pos = 3  # 按鈕開始位置由td[3]開始
 
-        driver.find_element(By.XPATH, "//*[@id='GridView1']/tbody/tr/td/table/tbody/tr/td[" + str(
-            button_pos) + "]").click()  # 尋找下一頁按鈕並點擊
+
+        driver.find_element(By.XPATH, "//*[@id='GridView1']/tbody/tr/td/table/tbody/tr/td[" + str(button_pos) + "]").click()  # 尋找下一頁按鈕並點擊
 
         if button_pos == 8 and not page_control or button_pos == 6 and page_control:  # 若是點擊(...)鍵則不讀取照片
             button_pos += 1
             continue
 
-        button_num = driver.find_element(By.XPATH, "//*[@id='GridView1']/tbody/tr/td/table/tbody/tr/td[" + str(
-            button_pos) + "]/span")  # 取得頁面之頁數 //*[@id="GridView1"]/tbody/tr/td/table/tbody/tr/td[4]/a
+        button_num = driver.find_element(By.XPATH, "//*[@id='GridView1']/tbody/tr/td/table/tbody/tr/td[" + str(button_pos) + "]/span")  # 取得頁面之頁數
         button_pos += 1
+        print("%d" % int(button_num.text), end='\r')  # 萬一因時間過長而被ban或突發原因可知當時跑到哪裡
 
         if int(button_num.text) == 1246 and broken_page:
             continue
@@ -114,12 +119,13 @@ def each(urlss, path, organize_class_name):
 
         with ThreadPoolExecutor() as executor:  # 多執行緒以減少下載圖片時間
             executor.map(getimage, image, organ, name, path_tmp)  # 同時下載圖片
-        time.sleep(1)  # 避免網站過度佔用時間 (暫定)
+        if int(button_num.text) == count and not page_big_six:
+            break
         if int(button_num.text) == count:  # 若頁面全部跑完則跳出迴圈
             break
-        time.sleep(3)  # 避免網站過度佔用時間 (暫定)
+        time.sleep(2.5)
     all_name.clear()  # 清空list
-    time.sleep(3)  # 避免網站過度佔用時間 (暫定)
+    time.sleep(3)
 
 
 # Chrome driver 前置作業
@@ -130,14 +136,16 @@ path_now = os.getcwd()  # 取得當前路徑
 mkdir(path_now + "/image")  # 在目前程式資料夾底下建立目錄名為image的資料夾
 
 main_page_requests = requests.get("https://collections.culture.tw/Class.aspx")  # 進入主網頁
-main_page_result = BeautifulSoup(main_page_requests.text, 'html.parser')  # 回傳網頁原始碼
+main_page_result = BeautifulSoup(main_page_requests.text, 'html.parser')  # 回傳網頁原始碼 -> 收集資料
 
 # start search
-all_main_class = main_page_result.select('div[class="col-12 mb-4"]')
-all_main_class.pop(0)
-for i in all_main_class:
-    title_elem = i.select('h2[class="ClassTypeTitle d-block mb-1"]')
+all_main_class = main_page_result.select('div[class="col-12 mb-4"]')  # 取得大分類之名稱
+all_main_class.pop(0)  # 刪除收集資料發現的空內容
+for i in all_main_class:  # 依序拜訪大分類下的小分類
+    title_elem = i.select('h2[class="ClassTypeTitle d-block mb-1"]')   # 取得大分類下所有小分類之名稱
+    print
     if not len(title_elem):
+        print("test")
         title_elem = i.select('h2[class="ClassTypeTitle d-block"]')
     # print(title_elem[0].text)
     mkdir(path_now + "/image/" + title_elem[0].text)
